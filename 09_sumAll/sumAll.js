a341a7c47eec99e6b36771daf65bb2068306bd49
istvantakacs <91099626+istvan-takacs@users.noreply.github.com>
2025-10-13 13:08:21 +0100

    Complete Javascript exercises 09-11
    Complete and test the functionality of sumAll,
    leapYears, and the two temp_Conversion funcitons.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+    if (!Number.isInteger(intOne) || (!Number.isInteger(intTwo))) {
+        return "ERROR"
+    }
+    if (intOne < 0 || intTwo < 0) {
+        return "ERROR"
+    }
+    let minInt = Math.min(intOne, intTwo);
+    let maxInt = Math.max(intOne, intTwo);
+    const arr = []
+    for (let i = minInt; i < maxInt + 1; i++) {
+        arr.push(i)
+    }
+    return arr.reduce((acc, value) => acc + value, 0)
 };
 
 // Do not edit below this line
