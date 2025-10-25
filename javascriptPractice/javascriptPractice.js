f90e729763503f84c8a4483eeb7c4e1ab977ec9e
istvantakacs <91099626+istvan-takacs@users.noreply.github.com>
2025-10-25 17:47:10 +0100

    Complete JS exercises about objects
    Practice array methods with map, sort, and
    reduce by accessing and manipulating object keys
    and values.

--- javascriptPractice/javascriptPractice.js
+++ javascriptPractice/javascriptPractice.js
@@ -32,3 +32,57 @@ function lastLetter(string) {
 }
 
 console.log(lastLetter("abcd"))
+
+
+/* You have an array of user objects, each one has name, surname and id.
+
+Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+ */
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map( (person) => {
+    return { fullName: person.name + " " + person.surname, id:person.id};
+})
+
+console.log(usersMapped);
+
+/* Map to names */
+john = { name: "John", age: 25 };
+pete = { name: "Pete", age: 30 };
+mary = { name: "Mary", age: 28 };
+
+users = [ john, pete, mary ];
+
+let names = users.map((user) => user.name);
+console.log(names);
+
+/* Sort users by age */
+john = { name: "John", age: 25 };
+pete = { name: "Pete", age: 30 };
+mary = { name: "Mary", age: 29 };
+
+let arr = [ pete, john, mary ];
+
+console.log(arr.sort((a,b) => a.age - b.age));
+
+// Get average age
+let avg = arr.reduce( (acc, user) => acc + user.age, 0) / users.length
+
+console.log(avg);
+
+// Create keyed object from array
+
+users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = users.reduce( (acc, user) => acc[user.id] = user, {});
+console.log(usersById)
