b4ac267428bf320279c05fae6384f268db608dcb
istvantakacs <91099626+istvan-takacs@users.noreply.github.com>
2025-10-31 15:06:36 +0000

    Complete JS exercise 16
    Complete the last JS exercise in this directory
    to the extent such that it passes all npm tests.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+// const getTheTitles = function(bookObjArr) {
+//     titles = [];
+//     for (let i = 0; i < bookObjArr.length; i++) {
+//         titles.push(bookObjArr[i].title);
+//     }
+//     return titles
+// };
 
-};
+const getTheTitles = bookObjArr =>  bookObjArr.reduce( (acc, curr) => acc.concat(curr.title), [])
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(person) {
+    return person.reduce( (oldest,curr) => {
+        const currentYear = new Date().getFullYear();
+        const ageCurr = (curr.yearOfDeath || currentYear) - curr.yearOfBirth;
+        const agePrev = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        return ageCurr > agePrev ? curr : oldest;
+    })
 };
 
 // Do not edit below this line
